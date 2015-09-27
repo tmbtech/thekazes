7cd663460c1821bb8259cee4ede86a7886bd08ae
Robbie dela Victoria <robert.delavictoria@nfl.com>
2015-09-27 14:49:06 -0700

    update login form

--- js/react/components/login.js
+++ js/react/components/login.js
@@ -6,9 +6,11 @@ export default class Login extends React.Component {
     this.firebaseRef = new Firebase("https://popping-fire-6215.firebaseio.com");
 
   }
+
   componentWillUnmount() {
     this.firebaseRef.off();
   }
+
   onClick = (e) => {
     e.preventDefault();
     const username = React.findDOMNode(this.refs.username).value;
@@ -31,22 +33,28 @@ export default class Login extends React.Component {
   }
 
   render() {
-      return (
-        <form>
-          <p>
-            Username: &nbsp;
-            <input type="text" ref="username" />
-          </p>
+    return (
+      <div className="page_content">
+        <div className="full_width_centered">
+          <div className="form_content">
+            <form>
+              <h3 className="form_subtitle">Admin login</h3>
 
-          <p>
-            Password:&nbsp;
-            <input type="password" ref="password" />
-          </p>
-
-          <p>
-            <button onClick={this.onClick}>click</button>
-          </p>
-        </form>
-      );
+              <div className="form_row_full">
+                <label>Username</label>
+                <input type="text" className="form_input required" ref="username" />
+              </div>
+              <div className="form_row_full">
+                <label>Password</label>
+                <input type="password" className="form_input required" ref="password" />
+              </div>
+              <div className="form_row_full">
+                <input type="submit" className="form_submit_contact" value="Enter" onClick={this.onClick}/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
